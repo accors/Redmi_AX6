ce8fd3cc52fbb7016d9129f5e7873692e36f1f41
accors <61780738+accors@users.noreply.github.com>
2021-07-23 12:01:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Add patch
+wget -P /openwrt/package/qca/nss/qca-nss-ecm-64/patches https://raw.githubusercontent.com/1715173329/nss-packages/main/qca/qca-nss-ecm/patches/205-resolve-high-load.patch
